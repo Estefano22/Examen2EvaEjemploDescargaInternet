7d1e35c87e12cc3299f629fe2e01ac51bfb3c750
Estefano22 <sandulescustefano8@gmail.com>
2021-03-14 19:53:45 +0100

    .

--- app/src/main/java/com/cbellmont/ejemplodescargainternet/Persoanjesstarswars.kt
+++ app/src/main/java/com/cbellmont/ejemplodescargainternet/Persoanjesstarswars.kt
@@ -4,8 +4,9 @@ import com.google.gson.annotations.SerializedName
 
 data class Persoanjesstarswars(
     var name: String,
+    val species: List<String>){
 
-    @SerializedName("species") val species: List<String>?) {
+
 
     override fun toString(): String {
         return " $name\n Y su especie  es $species\n\n"
